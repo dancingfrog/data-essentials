d35136fd60a39d2f844412567c94fe7d5845e2f1
John Hall <john@real-currents.com>
2019-06-29 13:18:41 -0400

    add r

---
+++ example.r
@@ -0,0 +1,8 @@
+mean(1:5)
+
+candidates <- c( Sys.getenv("R_PROFILE"),
+                 file.path(Sys.getenv("R_HOME"), "etc", "Rprofile.site"),
+                 Sys.getenv("R_PROFILE_USER"),
+                 file.path(getwd(), ".Rprofile") )
+
+Filter(file.exists, candidates)
--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
-source activate data_essentials
-
 # conda install -f -y -q -n py37 -c conda-forge --file requirements.txt
 
+# IPython/Jupyter
 conda install -y ipykernel
 python -m ipykernel install
 python -m ipykernel install --user
-python -m pip install 'python-language-server[all]'
+# Markdown jupyter kernel
 python -m pip install markdown-kernel
 python -m markdown_kernel.install
+# Python jupyter kernel
+python -m pip install 'python-language-server[all]'
+conda install -y r-essentials r-igraph
+rprofile="$(echo $(r  -f example.r  | grep '/Rprofile') | grep -o '[A-Z|a-z|\/][A-Z|a-z|0-9|\:|\/|\.]*')"
+echo 'options(repos = list(CRAN="http://cran.rstudio.com/"))' >> $rprofile
+# R jupyter kernel
+Rscript -e 'install.packages("languageserver")'
+Rscript -e 'IRkernel::installspec()'
+# Extra modules
 conda install -y matplotlib pandas geopandas koalas 'pyspark>=2.4' -c conda-forge
 conda install -y xeus xeus-python notebook -c conda-forge
 jupyter kernelspec list
