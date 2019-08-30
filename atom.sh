65f9b50c5777e195b5668c9c76bc909f2ac73345
John <john@real-currents.com>
2019-08-30 14:31:30 -0400

    improve conda init

--- atom.sh
+++ atom.sh
@@ -1,4 +1,20 @@
 unset PYTHONPATH;
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/jo088ha/miniconda3/condabin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/jo088ha/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/jo088ha/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/jo088ha/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
 conda activate data.essentials;
 apm install hydrogen hydrogen-launcher;
 apm install atom-ide-ui atom-language-r ide-python ide-r;
--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 unset PYTHONPATH
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/jo088ha/miniconda3/condabin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/jo088ha/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/jo088ha/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/jo088ha/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
 conda activate data.essentials
 
 conda update -n base -c defaults conda
