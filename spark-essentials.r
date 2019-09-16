10adf71e0b195a160b2d15bf358e529a95b6c3ff
John <john@real-currents.com>
2019-09-16 11:01:46 -0400

    add spark essentials for r

--- spark-essentials.r
+++ spark-essentials.r
@@ -1,6 +1,10 @@
 # Now let's create a simple list with 10000 integers
 # xrange() is more memory efficient so let's use that
-data = range(1, 10001)
+data <- 1:10001
 
 # now see how big our list is
-len(data)
+length(data)
+
+# Now let's create a DataFrame from our list
+frame <- data.frame(x = data)
+length(frame)
