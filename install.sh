1c1f7d4e0b2d715d8e2f37ec377567e470520528
John <john@real-currents.com>
2020-03-18 18:14:05 -0400

    udpate install and start learning r from books

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 unset PYTHONPATH
+PROJDIR=$CONDA_PREFIX
+PROJ_CPPFLAGS="-I$PROJDIR/include -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1"
+PROJ_LDFLAGS="-L$PROJDIR/lib"
+
+cp -r ./.R ~/
 
 conda update -y -c defaults conda
-conda install --force-reinstall -y conda-build
+
+conda install --force-reinstall -y conda conda-build
 #conda install -f -y -q -n py37 -c conda-forge --file requirements.txt
 
 # Install NodeJS & NPM
@@ -17,7 +23,7 @@ pip install dvc --upgrade
 #conda install --force-reinstall -y -c rdonnellyr r-base
 #conda install --force-reinstall -c rdonnellyr -y _r-mutex r-assertthat r-base64enc r-bh r-bitops r-boot r-car r-caret r-catools r-chron r-class r-cluster r-codetools r-colorspace r-crayon r-curl r-data.table r-dbi r-dichromat r-digest r-doparallel r-dplyr r-essentials r-evaluate r-foreach r-foreign r-formatr r-ggplot2 r-ggplot2movies r-gistr r-glmnet r-gridbase r-gtable r-hexbin r-highr r-htmltools r-htmlwidgets r-httpuv r-httr r-igraph r-irdisplay r-irkernel r-irlba r-iterators r-jsonlite r-kernsmooth r-knitr r-labeling r-lattice r-lazyeval r-lme4 r-magrittr r-maps r-markdown r-mass r-matrix r-matrixmodels r-memoise r-mgcv r-mime r-minqa r-munsell r-nlme r-nloptr r-nmf r-nnet r-openssl r-pbdzmq r-pbkrtest r-pkgmaker r-plyr r-pryr r-quantmod r-quantreg r-r6 r-randomforest r-rbokeh r-rcolorbrewer r-rcpp r-rcppeigen r-recommended r-registry r-repr r-reshape2 r-rmarkdown r-rngtools r-rpart r-reticulate r-scales r-shiny r-shinydashboard r-sparsem r-spatial r-stringi r-stringr r-survival r-tibble r-tidyr r-ttr r-uuid r-xtable r-xts r-yaml r-zoo
 # Update R profile
-export rprofile="$(echo $(r  -f example.r  | grep '/Rprofile') | grep -o '[A-Z|a-z|\/][A-Z|a-z|0-9|\:|\/|\.|\_]*')"
+export rprofile="$(echo $(R  -f example.r  | grep '/Rprofile') | grep -o '[A-Z|a-z|\/][A-Z|a-z|0-9|\:|\/|\.|\_]*')"
 echo $rprofile
 echo $(for rp in $rprofile; do echo 'options(repos = list(CRAN="http://cran.rstudio.com/"))' >> $rp; done;) || sudo echo $(for rp in $rprofile; do echo 'options(repos = list(CRAN="http://cran.rstudio.com/"))' >> $rp; done;)
 Rscript -e 'update.packages(repos="http://cran.rstudio.com/", ask=FALSE, checkBuilt=TRUE)'
