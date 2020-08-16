d54b6645161e23e341a8f576082b6c9368a026a9
John <john@real-currents.com>
2020-08-16 10:44:30 -0400

    update fortran example

--- example.f90
+++ example.f90
@@ -1,7 +1,15 @@
-    program example
+      !
+      ! This program writes 'Hello, World!' to the shell.
+      ! Version 0.1, September 27, 2017
+      !
+      program example
         
         !This is a comment
         
-        print*, "Hello world" !I’ve just printed something!
-    
-    end program example
+        print*, "Hello, World!" !I’ve just printed something!
+        
+        !
+        ! All done, goodbye
+        !
+      end program example
+      
